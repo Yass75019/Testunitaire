98660487ffa5d68684628a6b59d5afaa6914d407
Yacine SANAA <Yacine SANAA@172.16.0.166>
2014-01-17 15:38:53 +0100

    Class Point ajoutée

---
+++ TPTestsUnitaires/src/yacine/Point.java
@@ -0,0 +1,37 @@
+package yacine;
+
+import specification.InterfacePoint;
+
+public class Point implements InterfacePoint {
+
+	@Override
+	public int getOrd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAbs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setOrd(int ord) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAbs(int ord) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public InterfacePoint add(InterfacePoint p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
